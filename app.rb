b38a5845b7e0f10f5c9b3ddb66134a8dc78a9001
Danny C <35072063+dchen705@users.noreply.github.com>
2025-02-23 15:12:08 -0600

    Add routes and views

--- app.rb
+++ app.rb
@@ -27,6 +27,20 @@ get '/' do
   erb :home
 end
 
+get '/register' do
+  erb :register
+end
+
+post '/register' do; end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do; end
+
+post '/logout' do; end
+
 get '/characters' do
   @characters_list = @characters.list
   erb :characters
@@ -42,4 +56,26 @@ end
 
 post '/stack/remove' do
   @stack.delete(params[:id])
+end
+
+get '/decks' do
+  erb :decks
+end
+
+post '/deck/new' do; end
+
+get '/deck/edit/:id' do
+  id = params[:id]
+  erb :deck_edit
+end
+
+post '/deck/edit/:id' do; end
+
+post '/deck/remove' do; end
+
+get 'flashcards/:id' do
+  if params[:id] == 'stack'
+    @flashcards = @stack
+  end
+  erb :flashcards
 end
